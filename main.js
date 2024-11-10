2747e7027428a4c5b76c139b71a55c949c36e142
Júlio César Landim <127707529+devjuliolandim@users.noreply.github.com>
2024-11-10 16:22:08 -0300

    few lines

--- main.js
+++ main.js
@@ -4,28 +4,27 @@ const W = window.innerWidth;
 const H = window.innerHeight;
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 50, W/H, 0.1, 1000 );
+
+const camera = new THREE.PerspectiveCamera(45, W/H, 1,500);
+camera.position.set(0,0,100);
+camera.lookAt(0,0,0);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(W, H);
+document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const material = new THREE.LineBasicMaterial({color: 0x0000ff});
 
-camera.position.z = 5;
+const points = [];
 
-window.addEventListener( 'resize', () => {
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-});
+points.push(new THREE.Vector3(-10,0,0));
+points.push(new THREE.Vector3(0,10,0));
+points.push(new THREE.Vector3(0,0,10));
 
-function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render( scene, camera );
-}
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+const line = new THREE.Line(geometry, material);
+
+scene.add(line);
+renderer.render(scene, camera);
+
