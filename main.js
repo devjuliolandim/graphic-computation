9ea20ecd2b8064bbb272fe6bdcb53e8475448ab4
Júlio César Landim <127707529+devjuliolandim@users.noreply.github.com>
2024-11-10 15:50:41 -0300

    first rotanting cube

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-import * as THREE from "three";
+import * as THREE from 'three';
+
+const W = window.innerWidth;
+const H = window.innerHeight;
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 50, W/H, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+window.addEventListener( 'resize', () => {
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+});
+
+function animate() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render( scene, camera );
+}
